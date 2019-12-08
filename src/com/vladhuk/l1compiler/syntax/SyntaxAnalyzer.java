997e7f610b93e51c950f35bd16b88a2048218550
vladhuk <vladhookovskiy@gmail.com>
2019-12-08 21:40:44 +0200

    Refactor

--- src/com/vladhuk/l1compiler/syntax/SyntaxAnalyzer.java
+++ src/com/vladhuk/l1compiler/syntax/SyntaxAnalyzer.java
@@ -1,7 +1,6 @@
 package com.vladhuk.l1compiler.syntax;
 
 import com.vladhuk.l1compiler.lexical.Lexem;
-import com.vladhuk.l1compiler.lexical.Token;
 import com.vladhuk.l1compiler.util.Util;
 
 import java.io.File;
@@ -9,13 +8,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+
 
 public class SyntaxAnalyzer {
 
-    public static void analyze(List<String> tables) {
-        final List<Lexem> lexems = getLexemsFromTable(getLexemTableFromTables(tables));
+    public static void analyze(List<String> rows) {
+        final List<Lexem> lexems = Util.getLexemsFromLexemTable(Util.getSeparatedTables(rows).get(0));
 
         final Grammar grammar = new Grammar();
 
@@ -25,30 +23,6 @@ public class SyntaxAnalyzer {
             System.err.println("Syntax is wrong!");
             grammar.showError();
         }
-    }
-
-    private static List<Lexem> getLexemsFromTable(List<String> table) {
-        final Pattern stringDelimiter = Pattern.compile("('[^']*')|( +)");
-
-        return table.stream()
-                .map(row -> {
-                    final List<String> elements = Util.splitIncludingDelimiters(row, stringDelimiter).stream()
-                            .filter(str -> !str.isBlank())
-                            .collect(Collectors.toList());
-
-                    final int rowNumber = Integer.parseInt(elements.get(0));
-                    final String lexem = elements.get(1);
-                    final Token token = Token.valueOf(elements.get(2));
-
-                    return new Lexem(rowNumber, lexem, token);
-                })
-                .collect(Collectors.toList());
-    }
-
-    private static List<String> getLexemTableFromTables(List<String> tables) {
-        final String delimiter = tables.get(0);
-        final List<String> tablesWithoutFirstRow = tables.subList(1, tables.size());
-        return tablesWithoutFirstRow.subList(0, tablesWithoutFirstRow.indexOf(delimiter));
     }
 
     public static void analyze(Path tables) throws IOException {
--- src/com/vladhuk/l1compiler/util/Util.java
+++ src/com/vladhuk/l1compiler/util/Util.java
@@ -1,9 +1,14 @@
 package com.vladhuk.l1compiler.util;
 
+import com.vladhuk.l1compiler.lexical.Lexem;
+import com.vladhuk.l1compiler.lexical.Token;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 
 public class Util {
 
@@ -25,6 +30,40 @@ public class Util {
         }
 
         return resultList;
+    }
+
+    public static List<Lexem> getLexemsFromLexemTable(List<String> table) {
+        final Pattern stringDelimiter = Pattern.compile("('[^']*')|( +)");
+
+        return table.stream()
+                .map(row -> {
+                    final List<String> elements = Util.splitIncludingDelimiters(row, stringDelimiter).stream()
+                            .filter(str -> !str.isBlank())
+                            .collect(Collectors.toList());
+
+                    final int rowNumber = Integer.parseInt(elements.get(0));
+                    final String lexem = elements.get(1);
+                    final Token token = Token.valueOf(elements.get(2));
+
+                    return new Lexem(rowNumber, lexem, token);
+                })
+                .collect(Collectors.toList());
+    }
+
+
+    public static List<List<String>> getSeparatedTables(List<String> tablesRows) {
+        final String delimiter = tablesRows.get(0);
+        List<String> tablesWithoutFirstRow = tablesRows.subList(1, tablesRows.size());
+
+        final List<List<String>> splitTables = new ArrayList<>();
+
+        int indexOfDelimiter;
+        while ((indexOfDelimiter = tablesWithoutFirstRow.indexOf(delimiter)) != -1) {
+            splitTables.add(tablesWithoutFirstRow.subList(0, indexOfDelimiter));
+            tablesWithoutFirstRow = tablesWithoutFirstRow.subList(indexOfDelimiter + 1, tablesWithoutFirstRow.size());
+        }
+
+        return splitTables;
     }
 
 }
