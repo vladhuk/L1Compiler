8f91369b9b4ca45a910711a27f06a5f745309ac6
vladhuk <vladhookovskiy@gmail.com>
2019-12-12 01:09:27 +0200

    Finish automatic

--- src/com/vladhuk/l1compiler/automat/Automatic.java
+++ src/com/vladhuk/l1compiler/automat/Automatic.java
@@ -11,7 +11,8 @@ import java.util.List;
 public class Automatic {
 
     private static final List<State> statesWhiteList = Arrays.asList(
-            State.ERROR, State.EXIT, State.ARITHM_OR_BOOL_SIGN, State.ARITHM_SIGN, State.BOOL_SIGN
+            State.ERROR, State.EXIT, State.EOL, State.ARITHM_OR_BOOL_SIGN, State.ARITHM_SIGN, State.BOOL_SIGN,
+            State.END
     );
 
     private LinkedList<String> localStorageStack = new LinkedList<>();
@@ -26,13 +27,22 @@ public class Automatic {
         return automatic;
     }
 
+    private void showExpectedError(String name, int rowNumber) {
+        System.err.println("Expected '" + name + "' on line " + rowNumber + '.');
+    }
+
+    private void showUnexpectedError(Lexem lexem) {
+        final String lexemName = lexem.getName().equals("\n") ? "new line" : lexem.getName();
+        System.err.println("Unexpected '" + lexemName + "' on line " + lexem.getRowNumber() + '.');
+    }
+
     public boolean run() {
-        State currentState = manageState(State.EXIT);
+        State currentState = State.EXIT;
 
         while (true) {
             currentState = manageState(currentState);
 
-            if (lexems.size() == 0 && currentState == State.EXIT) {
+            if (lexems.size() == 0 && stateStack.size() == 0 && currentState == State.EXIT) {
                 return true;
             } else if (currentState == State.ERROR) {
                 return false;
@@ -42,7 +52,12 @@ public class Automatic {
 
     private State getState(Lexem lexem) {
         switch (lexem.getToken()) {
-            case DECLARATION: return lexem.getName().equals("val") ? State.START_DECLARATION_VAL : State.START_DECLARATION_VAR;
+            case DECLARATION: return processDeclaration();
+            case IDENTIFIER: return processIdentifier();
+            case JUMP: return State.GOTO;
+            case CONDITION: return State.IF;
+            case LOOP: return State.LOOP;
+
             default: return State.ERROR;
         }
     }
@@ -55,16 +70,13 @@ public class Automatic {
         switch (state) {
             case EXIT: return performExit();
             case EOL: return performEol();
-            case START_DECLARATION_VAL: return performStartDeclarationVal();
-            case START_DECLARATION_VAR: return performStartDeclarationVar();
-            case VAL_OR_VAR: return performValOrVar();
-            case DECLARATION_IDENTIFIER: return performDeclarationIdentifier();
+            case DECLARATION: return performDeclaration();
             case DECLARATION_OPTIONAL_PUNCT: return performDeclarationOptionalPunct();
             case TYPE: return performType();
             case ASSIGN: return performAssign();
             case OPTIONAL_ASSIGN: return performOptionalAssign();
-            case EXPRESSION_OR_IDENT: return performExpressionOrIdent();
             case EXPRESSION: return performExpression();
+            case EXPRESSION_OR_STRING: return performExpressionOrString();
             case STRING: return performString();
             case ARITHM_OR_BOOL_SIGN: return performArithmOrBoolSign();
             case BOOL_SIGN: return performBoolSign();
@@ -74,20 +86,34 @@ public class Automatic {
             case CLOSE_BRACKET: return performCloseBracket();
             case CLOSE_BRACKET_ARITHM: return performCloseBracketArithm();
             case CLOSE_BRACKET_BOOL: return performCloseBracketBool();
+            case LABEL_MARK_OR_ASSIGNING: return performLabelMarkOrAssigning();
+            case IF: return performIf();
+            case THEN: return performThen();
+            case IDENTIFIER: return performIdentifier();
+            case GOTO: return performGoto();
+            case LOOP: return performLoop();
+            case TO: return performTo();
+            case DO: return performDo();
+            case DECLARATION_OR_ASSIGNING: return performDeclarationOrAssigning();
+            case END: return performEnd();
 
-            default: return State.ERROR;
+            case ERROR:
+            default:
+                return State.ERROR;
         }
     }
 
     private State performExit() {
         final State state;
 
-        if (stateStack.size() != 0) {
+        if (stateStack.size() != 0 && stateStack.peek() != State.END) {
             state = stateStack.poll();
+        } else if (lexems.size() == 0 && stateStack.size() > 0) {
+            state = State.ERROR;
+        } else if (lexems.size() == 0) {
+            state = State.EXIT;
         } else {
-            state = lexems.size() == 0
-                    ? State.EXIT
-                    : getState(lexems.poll());
+            state = getState(lexems.peek());
         }
 
         if (state == State.EXIT || state == State.ERROR) {
@@ -102,33 +128,32 @@ public class Automatic {
 
         if (lexem == null || lexem.getName().equals("\n")) {
             return State.EXIT;
-        } else {
-            return State.ERROR;
         }
+
+        showExpectedError("new line", lexem.getRowNumber());
+        return State.ERROR;
     }
 
-    private State performStartDeclarationVal() {
+    private State processDeclaration() {
         stateStack.push(State.EOL);
-        localStorageStack.push("val");
-        return State.VAL_OR_VAR;
+        return State.DECLARATION;
     }
 
-    private State performStartDeclarationVar() {
-        stateStack.push(State.EOL);
-        localStorageStack.push("var");
-        return State.VAL_OR_VAR;
-    }
+    private State performDeclaration() {
+        final Lexem lexem = lexems.poll();
 
-    private State performValOrVar() {
-        return State.DECLARATION_IDENTIFIER;
-    }
-
-    private State performDeclarationIdentifier() {
-        if (lexems.poll().getToken() == Token.IDENTIFIER) {
-            return State.DECLARATION_OPTIONAL_PUNCT;
-        } else {
-            return State.ERROR;
+        if (lexem.getName().equals("val")) {
+            localStorageStack.push("val");
+            stateStack.push(State.DECLARATION_OPTIONAL_PUNCT);
+            return State.IDENTIFIER;
         }
+        if (lexem.getName().equals("var")) {
+            localStorageStack.push("var");
+            stateStack.push(State.DECLARATION_OPTIONAL_PUNCT);
+            return State.IDENTIFIER;
+        }
+
+        return State.ERROR;
     }
 
     private State performDeclarationOptionalPunct() {
@@ -137,30 +162,39 @@ public class Automatic {
         if (lexem.getToken() == Token.PUNCT) {
             return State.TYPE;
         } else if (lexem.getToken() == Token.ASSIGN) {
-            return State.EXPRESSION_OR_IDENT;
-        } else {
-            return State.ERROR;
+            stateStack.push(State.EXIT);
+            return State.EXPRESSION_OR_STRING;
         }
+
+        showUnexpectedError(lexem);
+        return State.ERROR;
     }
 
     private State performType() {
-        if (lexems.poll().getToken() == Token.TYPE) {
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getToken() == Token.TYPE) {
             if (localStorageStack.poll().equals("val")) {
                 return State.ASSIGN;
             } else {
                 return State.OPTIONAL_ASSIGN;
             }
-        } else {
-            return State.ERROR;
         }
+
+        showExpectedError("type", lexem.getRowNumber());
+        return State.ERROR;
     }
 
     private State performAssign() {
-        if (lexems.poll().getToken() == Token.ASSIGN) {
-            return State.EXPRESSION_OR_IDENT;
-        } else {
-            return State.ERROR;
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getToken() == Token.ASSIGN) {
+            stateStack.push(State.EXIT);
+            return State.EXPRESSION_OR_STRING;
         }
+
+        showExpectedError("=", lexem.getRowNumber());
+        return State.ERROR;
     }
 
     private State performOptionalAssign() {
@@ -171,16 +205,22 @@ public class Automatic {
         }
     }
 
-    private State performExpressionOrIdent() {
-        stateStack.push(State.EXIT);
+    private State performExpressionOrString() {
+        if (lexems.peek().getName().matches("'[^']*'")) {
+            return State.STRING;
+        }
+
         return State.EXPRESSION;
     }
 
     private State performExpression() {
-        final Lexem lexem = lexems.peek();
+        final Lexem lexem = lexems.poll();
 
+        if (lexem.getToken() == Token.ADD_OP) {
+            stateStack.push(State.ARITHM_OR_BOOL_SIGN);
+            return State.EXPRESSION;
+        }
         if (lexem.getToken() == Token.IDENTIFIER) {
-            lexems.poll();
             return State.ARITHM_OR_BOOL_SIGN;
         }
         if (lexem.getToken() != Token.CONSTANT && lexem.getToken() != Token.BRACKET_OP) {
@@ -188,20 +228,14 @@ public class Automatic {
         }
         if (lexem.getName().equals("(")) {
             stateStack.push(State.CLOSE_BRACKET);
-            lexems.poll();
             return State.EXPRESSION;
         }
-        if (lexem.getName().matches("true|false")) {
-            return State.BOOL_EXPRESSION;
-        }
-        if (lexem.getName().matches("'[^']*'")) {
-            return State.STRING;
-        }
 
-        return State.ARITHM_EXPRESSION;
+        return State.ARITHM_OR_BOOL_SIGN;
     }
 
     private State performString() {
+        lexems.poll();
         return State.EXIT;
     }
 
@@ -240,8 +274,10 @@ public class Automatic {
         if (lexems.size() == 0) {
             return State.EXIT;
         }
-        if (lexems.peek().getToken() == Token.REL_OP) {
-            lexems.poll();
+
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getToken() == Token.REL_OP) {
             return State.BOOL_EXPRESSION;
         }
 
@@ -251,6 +287,10 @@ public class Automatic {
     private State performArithmExpression() {
         final Lexem lexem = lexems.poll();
 
+        if (lexem.getToken() == Token.ADD_OP) {
+            stateStack.push(State.ARITHM_SIGN);
+            return State.ARITHM_EXPRESSION;
+        }
         if (lexem.getName().equals("(")) {
             stateStack.push(State.CLOSE_BRACKET_ARITHM);
             return State.ARITHM_EXPRESSION;
@@ -259,20 +299,26 @@ public class Automatic {
             return State.ARITHM_SIGN;
         }
 
+        showUnexpectedError(lexem);
         return State.ERROR;
     }
 
     private State performBoolExpression() {
         final Lexem lexem = lexems.poll();
 
+        if (lexem.getToken() == Token.ADD_OP) {
+            stateStack.push(State.ARITHM_SIGN);
+            return State.ARITHM_EXPRESSION;
+        }
         if (lexem.getName().equals("(")) {
             stateStack.push(State.CLOSE_BRACKET_BOOL);
             return State.ARITHM_EXPRESSION;
         }
-        if (lexem.getToken() == Token.IDENTIFIER || lexem.getName().matches("true|false")) {
+        if (lexem.getToken() == Token.IDENTIFIER || lexem.getToken() == Token.CONSTANT) {
             return State.BOOL_SIGN;
         }
 
+        showUnexpectedError(lexem);
         return State.ERROR;
     }
 
@@ -286,6 +332,149 @@ public class Automatic {
 
     private State performCloseBracketBool() {
         return lexems.poll().getName().equals(")") ? State.BOOL_SIGN : State.ERROR;
+    }
+
+    private State performLabelMarkOrAssigning() {
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getToken() == Token.PUNCT) {
+            return State.EOL;
+        } else if (lexem.getToken() == Token.ASSIGN) {
+            stateStack.push(State.EOL);
+            return State.EXPRESSION_OR_STRING;
+        }
+
+        showUnexpectedError(lexem);
+        return State.ERROR;
+    }
+
+    private State performGoto() {
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getToken() == Token.JUMP) {
+            stateStack.push(State.EOL);
+            return State.IDENTIFIER;
+        }
+
+        showExpectedError("goto", lexem.getRowNumber() - 1);
+        return State.ERROR;
+    }
+
+    private State performIf() {
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getName().equals("if")) {
+            stateStack.push(State.THEN);
+            return State.BOOL_EXPRESSION;
+        }
+
+        showExpectedError("if", lexem.getRowNumber());
+        return State.ERROR;
+    }
+
+    private State performThen() {
+        while (lexems.peek().getName().equals("\n")) {
+            lexems.poll();
+        }
+
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getName().equals("then")) {
+            stateStack.push(State.GOTO);
+            return State.EOL;
+        }
+
+        showExpectedError("then", lexem.getRowNumber());
+        return State.ERROR;
+    }
+
+    private State processIdentifier() {
+        stateStack.push(State.LABEL_MARK_OR_ASSIGNING);
+        return State.IDENTIFIER;
+    }
+
+    private State performIdentifier() {
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getToken() == Token.IDENTIFIER) {
+            return State.EXIT;
+        }
+
+        showExpectedError("identifier", lexem.getRowNumber());
+        return State.ERROR;
+    }
+
+    private State performLoop() {
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getName().equals("while")) {
+            stateStack.push(State.DO);
+            return State.BOOL_EXPRESSION;
+        }
+        if (lexem.getName().equals("for")) {
+            return State.DECLARATION_OR_ASSIGNING;
+        }
+        if (lexem.getName().equals("end")) {
+            return State.END;
+        }
+
+        showUnexpectedError(lexem);
+        return State.ERROR;
+    }
+
+    private State performDeclarationOrAssigning() {
+        final Lexem lexem = lexems.peek();
+
+        if (lexem.getToken() == Token.IDENTIFIER) {
+            stateStack.push(State.TO);
+            lexems.poll();
+            return State.ASSIGN;
+        }
+        if (lexem.getToken() == Token.DECLARATION) {
+            stateStack.push(State.TO);
+            return State.DECLARATION;
+        }
+
+        showExpectedError("declaration or assigning", lexem.getRowNumber());
+        return State.ERROR;
+    }
+
+    private State performTo() {
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getName().equals("to")) {
+            stateStack.push(State.DO);
+            return State.ARITHM_EXPRESSION;
+        }
+
+        showExpectedError("to", lexem.getRowNumber());
+        return State.ERROR;
+    }
+
+    private State performDo() {
+        while (lexems.peek().getName().equals("\n")) {
+            lexems.poll();
+        }
+
+        final Lexem lexem = lexems.poll();
+
+        if (lexem.getName().equals("do")) {
+            stateStack.push(State.END);
+            return State.EOL;
+        }
+
+        showExpectedError("do", lexem.getRowNumber());
+        return State.ERROR;
+    }
+
+
+    private State performEnd() {
+        if (stateStack.peek() == State.END) {
+            stateStack.poll();
+            return State.EOL;
+        }
+
+        return State.ERROR;
     }
 
 }
