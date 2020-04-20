dd2d7b7850616163a871eb442e8e0dbe061c7051
vladhuk <vladhookovskiy@gmail.com>
2020-04-21 01:31:29 +0300

    Improve util

--- src/com/vladhuk/l1compiler/util/Util.java
+++ src/com/vladhuk/l1compiler/util/Util.java
@@ -1,9 +1,11 @@
 package com.vladhuk.l1compiler.util;
 
 import com.vladhuk.l1compiler.lexical.Lexem;
+import com.vladhuk.l1compiler.lexical.Pair;
 import com.vladhuk.l1compiler.lexical.Token;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,6 +13,8 @@ import java.util.stream.Collectors;
 
 
 public class Util {
+
+    private static final Pattern STRING_DELIMITER = Pattern.compile("('[^']*')|( +)");
 
     public static List<String> splitIncludingDelimiters(String text, Pattern regex) {
         final List<String> resultList = new ArrayList<>();
@@ -33,37 +37,59 @@ public class Util {
     }
 
     public static List<Lexem> getLexemsFromLexemTable(List<String> table) {
-        final Pattern stringDelimiter = Pattern.compile("('[^']*')|( +)");
-
         return table.stream()
                 .map(row -> {
-                    final List<String> elements = Util.splitIncludingDelimiters(row, stringDelimiter).stream()
+                    final List<String> elements = Util.splitIncludingDelimiters(row, STRING_DELIMITER).stream()
                             .filter(str -> !str.isBlank())
                             .collect(Collectors.toList());
 
                     final int rowNumber = Integer.parseInt(elements.get(0));
                     final String lexem = elements.get(1);
                     final Token token = Token.valueOf(elements.get(2));
+                    final int index = elements.size() > 3 ? Integer.valueOf(elements.get(3)) : -1;
 
-                    return new Lexem(rowNumber, lexem, token);
+                    return new Lexem(rowNumber, lexem, token, index);
                 })
                 .collect(Collectors.toList());
     }
 
+    public static List<Pair> getPairsFromTable(List<String> table) {
+        return table.stream()
+                .map(row -> {
+                    final List<String> elements = Util.splitIncludingDelimiters(row, STRING_DELIMITER).stream()
+                            .filter(str -> !str.isBlank())
+                            .collect(Collectors.toList());
+
+                    final String name = elements.get(0);
+                    final Pair.Type type = Pair.Type.valueOf(elements.get(1));
+                    final String value = elements.get(2).equals(Pair.UNDEF) ? Pair.UNDEF : elements.get(2);
+                    final boolean modifiable = Boolean.parseBoolean(elements.get(3));
+                    final int index = Integer.parseInt(elements.get(4));
+
+                    return new Pair(name, type, value, modifiable, index);
+                })
+                .collect(Collectors.toList());
+    }
 
     public static List<List<String>> getSeparatedTables(List<String> tablesRows) {
-        final String delimiter = tablesRows.get(0);
-        List<String> tablesWithoutFirstRow = tablesRows.subList(1, tablesRows.size());
+        final String delimiter = tablesRows.get(tablesRows.size() - 1);
+        List<String> tempTableRows = tablesRows;
 
         final List<List<String>> splitTables = new ArrayList<>();
 
         int indexOfDelimiter;
-        while ((indexOfDelimiter = tablesWithoutFirstRow.indexOf(delimiter)) != -1) {
-            splitTables.add(tablesWithoutFirstRow.subList(0, indexOfDelimiter));
-            tablesWithoutFirstRow = tablesWithoutFirstRow.subList(indexOfDelimiter + 1, tablesWithoutFirstRow.size());
+        while ((indexOfDelimiter = tempTableRows.indexOf(delimiter)) != -1) {
+            splitTables.add(tempTableRows.subList(0, indexOfDelimiter));
+            tempTableRows = tempTableRows.subList(indexOfDelimiter + 1, tempTableRows.size());
         }
 
         return splitTables;
+    }
+
+    public static String tableToString(Collection<?> table) {
+        return table.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining("\n"));
     }
 
 }
